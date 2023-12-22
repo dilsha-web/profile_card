d0f558ed6564ea47dba595d7ba7165bdf8dfbef1
dilsha-web <rameshi.wijesinghe@gmail.com>
2023-12-22 22:46:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let Follow = document.getElementById("follow")
+
+Follow.addEventListener("click",follow_me)
+
+function follow_me(event){
+    alert("Thank you for following me");
+}
+
+let Message = document.getElementById("msg")
+
+Message.addEventListener("click",message_me)
+
+function message_me(event){
+    alert("Thank you for texting me");
+}
